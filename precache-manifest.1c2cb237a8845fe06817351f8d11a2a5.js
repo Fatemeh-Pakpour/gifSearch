f64b4319090839161afaf6cf1dd54f9ccc5b85c3
Fatemeh Pakpour <fatemehpakpour64@gmail.com>
2019-11-06 22:10:49 +0100

    Updates

---
+++ precache-manifest.1c2cb237a8845fe06817351f8d11a2a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2af1544a8a82007587c058c72e29ed1",
+    "url": "/gifSearch/index.html"
+  },
+  {
+    "revision": "eb47317451d2481d9cf9",
+    "url": "/gifSearch/static/css/main.4f725bb8.chunk.css"
+  },
+  {
+    "revision": "64b26249a61e505568cf",
+    "url": "/gifSearch/static/js/2.6fa7896b.chunk.js"
+  },
+  {
+    "revision": "eb47317451d2481d9cf9",
+    "url": "/gifSearch/static/js/main.17a353f5.chunk.js"
+  },
+  {
+    "revision": "294d79201c8d2e57cdd5",
+    "url": "/gifSearch/static/js/runtime-main.e7f3a276.js"
+  }
+]);
